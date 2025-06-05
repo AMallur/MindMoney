22635711524086cdce11ff9ac0acec6790cdfbac
AMallur <127694065+AMallur@users.noreply.github.com>
2025-06-05 14:20:33 -0500

    Create New.swift

---
+++ AccountingApp/New.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            TransactionsView()
+                .tabItem {
+                    Label("Transactions", systemImage: "list.bullet.rectangle")
+                }
+            CoursesView()
+                .tabItem {
+                    Label("Courses", systemImage: "book")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
